c4907559e77ad7f29e86c12961e2886fe6b762c9
Aluno SENAI <Aluno@SN-358555.edu.fiesc.com.br>
2025-04-14 15:08:59 -0300

    criando paginas das matérias a comessando o css e js

---
+++ portfolio.js
@@ -0,0 +1,34 @@
+
+function botao_materias(){
+
+}
+function botao_outros(){
+
+}
+function botao_curriculo(){
+
+}
+function botao_matematica(){
+
+}
+function botao_natureza(){
+
+}
+function botao_humanas(){
+    
+}
+function botao_linguagens(){
+    
+}
+function botao_voltar_materias(){
+    
+}
+function botao_sobre_mim(){
+    
+}
+function botao_voltar_inicio(){
+    
+}
+function botao_ (){
+    
+}
